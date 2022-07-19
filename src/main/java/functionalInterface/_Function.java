2386901af03b15b9c580a6c0959ed611d7f0fb5b
sudoaptgetsmart <96799397+sudoaptgetsmart@users.noreply.github.com>
2022-07-19 11:10:57 -0500

    Added multiplyBy10Function and then combined it with incrementByOneFunction

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -10,10 +10,20 @@ public class _Function {
 		int increment2 = incrementByOneFunction.apply(1);
 		System.out.println(increment2);
 
+		int multiply = multiplyBy10Function.apply(increment2);
+		System.out.println(multiply);
+
+
+		// Combines the two functions
+		Function<Integer, Integer> addBy1AndThenMultipleBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+		System.out.println(addBy1AndThenMultipleBy10.apply(1));
+
 	}
 
 	// First Integer corresponds to input type and second Integer corresponds to return type.
 	static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+	static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
 
 	// Same as above.
 	static int incrementByOne(int number) {
