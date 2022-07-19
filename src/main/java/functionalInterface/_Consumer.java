208aa1e9d1931d112d803e995f075e69def9d102
sudoaptgetsmart <96799397+sudoaptgetsmart@users.noreply.github.com>
2022-07-19 14:17:36 -0500

    Added BiConsumer

--- src/main/java/functionalInterface/_Consumer.java
+++ src/main/java/functionalInterface/_Consumer.java
@@ -1,27 +1,46 @@
 package functionalInterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 // Consumer: Accepts a single input argument and returns no result.
 public class _Consumer {
 
+	static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) -> System.out.println(
+			"Hello, " + customer.customerName +
+					"thanks for registering phone number " + (
+					showPhoneNumber ? customer.customerPhoneNumber :
+							"*******"
+			));
+	static Consumer<Customer> greetCustomerConsumer = customer -> System.out.println(
+			"Hello, " + customer.customerName +
+					"thanks for registering phone number " + customer.customerPhoneNumber);
+
 	public static void main(String[] args) {
 		// Normal Java
 		Customer maria = new Customer("Maria", "999999");
 		greetCustomer(maria);
+		greetCustomerV2(maria, false);
 
 		// Consumer Functional interface
 		greetCustomerConsumer.accept(maria);
-	}
 
-	static Consumer<Customer> greetCustomerConsumer = customer -> System.out.println(
-			"Hello, " + customer.customerName +
-					"thanks for registering phone number " + customer.customerPhoneNumber);
+		greetCustomerConsumerV2.accept(maria, false);
+	}
 
 	static void greetCustomer(Customer customer) {
 		System.out.println(
 				"Hello, " + customer.customerName +
 						"thanks for registering phone number " + customer.customerPhoneNumber);
+	}
+
+	static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+		System.out.println(
+				"Hello, " + customer.customerName +
+						"thanks for registering phone number " + (
+						showPhoneNumber ? customer.customerPhoneNumber :
+								"*******"
+				));
 	}
 
 
