f4620b7ca6be5d44e1c6656d71de62d23a09e99a
sudoaptgetsmart <96799397+sudoaptgetsmart@users.noreply.github.com>
2022-07-19 11:41:48 -0500

    Added BiFunction

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -1,9 +1,15 @@
 package functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
+	// First Integer corresponds to input type and second Integer corresponds to return type.
+	static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+	static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
 	public static void main(String[] args) {
+		// Function takes 1 argument and produces 1 result
 		int increment = incrementByOne(1);
 		System.out.println(increment);
 
@@ -15,18 +21,24 @@ public class _Function {
 
 
 		// Combines the two functions
-		Function<Integer, Integer> addBy1AndThenMultipleBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
-		System.out.println(addBy1AndThenMultipleBy10.apply(1));
+		Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+		System.out.println(addBy1AndThenMultiplyBy10.apply(1));
+
+		// BiFunction takes 2 arguments and produces 1 result
+		System.out.println(incrementByOneAndMultiply(4, 100));
+		System.out.println(incrementByOneAndMultiplyBiFunction.apply(4, 100));
 
 	}
 
-	// First Integer corresponds to input type and second Integer corresponds to return type.
-	static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
-
-	static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+	static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+			(numberToIncrementByOne, numberToMultiplyBy) -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
 
 	// Same as above.
 	static int incrementByOne(int number) {
 		return number + 1;
+	}
+
+	static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+		return (number + 1) * numToMultiplyBy;
 	}
 }
